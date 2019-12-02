86b208694b6bccf46cef759f73cf150b3e486900
Dmitry Vasilevich <wotlk777@gmail.com>
2019-12-03 01:47:02 +0300

    Add project files.

---
+++ Udemy.Dating.Api/Data/DatingContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Udemy.Dating.Api.Models;
+
+namespace Udemy.Dating.Api.Data
+{
+    public class DatingContext : DbContext
+    {
+        public DatingContext(DbContextOptions<DatingContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
