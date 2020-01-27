7482913733289f87b5324d6d32c2c85604e4c31f
Dmitry Vasilevich <wotlk777@gmail.com>
2020-01-27 21:55:23 +0300

    added static routes class

---
+++ Udemy.Dating.Api/Contracts/V1/ApiRoutes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Udemy.Dating.Api.Contracts.V1
+{
+    public static class ApiRoutes
+    {
+        public const string Root = "api";
+        public const string Version = "v1";
+        public const string Base = Root + "/" + Version;
+
+        public static class Identity
+        {
+            public const string Login = Base + "/identity/login";
+            public const string Register = Base + "/identity/register";
+        }
+
+        public static class Azure
+        {
+            public const string Base = ApiRoutes.Base + "azure";
+            public const string KeyVault = Root + "keyvault";
+        }
+    }
+}
--- Udemy.Dating.Api/Controllers/AzureController.cs
+++ Udemy.Dating.Api/Controllers/AzureController.cs
@@ -3,12 +3,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Udemy.Dating.Api.Contracts.V1;
 using Udemy.Dating.Application.Interfaces;
 
 namespace Udemy.Dating.Api.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
     public class AzureController : ControllerBase
     {
         private readonly IAzureRepository _azure;
@@ -18,13 +18,13 @@ namespace Udemy.Dating.Api.Controllers
             _azure = azure;
         }
 
-        [HttpGet]
+        [HttpGet(ApiRoutes.Azure.Base)]
         public IActionResult Get()
         {
             return Ok("test");
         }
 
-        [HttpGet("keyvault")]
+        [HttpGet(ApiRoutes.Azure.KeyVault)]
         public async Task<IActionResult> GetKeyVaultValue([FromQuery]string secretId)
         {
             return Ok(await _azure.GetKeyVaultSecretAsync(secretId));
